b87aabe92066c96990329933066d996b89dacbe0
gorazdkotnik <gorazdkotnik05@gmail.com>
2022-12-21 10:13:34 +0100

    moved graph drawing functions into seperate folder

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from languagetext import LanguageText
 from fileinput import FileInput
 from frequency import Frequency
 from ngrams import classify_ngrams
-import matplotlib.pyplot as plt
-from matplotlib.lines import Line2D
+from graphs import draw_distance_bar_chart, draw_top_ngrams_bar_chart
 
 ROOT_TEXT_FILES_DIRECTORY = 'languages'
 NGRAMS_RANGE = range(2, 5)
@@ -42,42 +41,6 @@ def main():
     draw_top_ngrams_bar_chart('slovene', slo_frequency.get_frequency_table())
     draw_top_ngrams_bar_chart('croatian', hr_frequency.get_frequency_table())
     draw_top_ngrams_bar_chart('english', en_frequency.get_frequency_table())
-
-
-def draw_distance_bar_chart(language_distances):
-    for language_distance in language_distances:
-        if language_distances[language_distance] == min(language_distances.values()):
-            plt.bar(language_distance, language_distances[language_distance], color='green', width=0.5)
-        else:
-            plt.bar(language_distance, language_distances[language_distance], color='red', width=0.5)
-
-    plt.xticks(range(len(language_distances)), language_distances.keys())
-
-    plt.title('Distance between input text and language n-grams')
-    plt.xlabel('Language')
-    plt.ylabel('Distance')
-
-    legend_elements = [Line2D([0], [0], color='green', lw=4, label='Predicted language'),
-                       Line2D([0], [0], color='red', lw=4, label='Other languages')]
-    plt.legend(handles=legend_elements)
-
-    plt.show()
-
-
-def draw_top_ngrams_bar_chart(title, language_ngrams_dict):
-    top_ngrams = dict(list(language_ngrams_dict.items())[:10])
-    top_ngrams = dict(sorted(top_ngrams.items(), key=lambda item: item[1], reverse=False))
-
-    for ngram in top_ngrams:
-        plt.barh(ngram, top_ngrams[ngram])
-
-    plt.yticks(range(len(top_ngrams)), top_ngrams.keys())
-
-    plt.title('Top 10 n-grams for ' + title + ' language')
-    plt.xlabel('Frequency')
-    plt.ylabel('N-gram')
-
-    plt.show()
 
 
 if __name__ == '__main__':
